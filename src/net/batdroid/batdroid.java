1d6d73431acee6def6ccd580c82d20bdc63e0b96
chippytdm <chippytdm@a2b22b0b-d01f-a98a-cb79-448e59e97dde>
2010-07-03 14:56:37 +0000

    Initial import of batdroid android app sources

---
+++ src/net/batdroid/batdroid.java
@@ -0,0 +1,39 @@
+package net.batdroid;
+
+import java.io.IOException;
+
+import android.R.drawable;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.app.Activity;
+
+public class batdroid extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
