9c0dbe55129fed8170cd47912ce94fc771ec06e9
chippytdm <chippytdm@a2b22b0b-d01f-a98a-cb79-448e59e97dde>
2010-07-03 18:18:16 +0000

    Start/Stop tether interface(initial framework)

--- trunk/src/net/batdroid/batdroid.java
+++ trunk/src/net/batdroid/batdroid.java
@@ -26,14 +26,53 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
 import android.app.Activity;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 
-public class batdroid extends Activity
+
+
+public class batdroid extends Activity implements OnClickListener
 {
+        public static final String MSG_TAG = "BATDROID -> MainActivity";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	// Capture our button from layout
+    	ImageView button_start = (ImageView)findViewById(R.id.startTetherBtn);
+    	// Register the onClick listener with the implementation above
+    	button_start.setOnClickListener(this);
+        // Capture our button from layout
+    	ImageView button_stop = (ImageView)findViewById(R.id.stopTetherBtn);
+    	// Register the onClick listener with the implementation above
+    	button_stop.setOnClickListener(this);
+    }
+
+
+    // Implement the OnClickListener callback
+    public void onClick(View v) {
+    
+    	int id = v.getId();
+    	switch(id){
+    		case R.id.startTetherBtn:{
+                        Log.d(MSG_TAG, "StartBtn pressed ...");
+    			TextView test = (TextView)findViewById(R.id.text_start);
+		    	test.setText("Processed event to start tether.");	
+    		}break;
+    		case R.id.stopTetherBtn:{
+                        Log.d(MSG_TAG, "StopBtn pressed ...");
+    			TextView test = (TextView)findViewById(R.id.text_start);
+		    	test.setText("Processed event to stop tether.");	
+    		}break;
+    		default:{
+    		//error handling here
+    		}
+    	}
+        
     }
 }
